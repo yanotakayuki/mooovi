1a7d2596ee42aad34890fe43f111e89552190a77
矢野高惟 <yanot3151@gmail.com>
2018-12-19 10:50:29 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 TechReviewSite::Application.routes.draw do
   resources :products, only: :show do
+    resources :reviews, only: [:new, :create]
     collection do
       get 'search'
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_073334) do
+ActiveRecord::Schema.define(version: 2018_12_18_083427) do
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2018_12_18_073334) do
     t.string "director"
     t.text "detail"
     t.string "open_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname"
+    t.integer "rate"
+    t.text "review"
+    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
